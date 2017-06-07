925c2b161313b558300b3c208e56f3a511f6f0fc
iyke <nderiony95@gmail.com>
2017-06-07 12:27:23 +0300

    updated index js adding images

--- app/routes/index.js
+++ app/routes/index.js
@@ -11,10 +11,9 @@ var news = [{
   id: 2,
   headline: "How Jubilee WON the election",
   description: "After a lanslide victory Jubilee coalition was elected back to power for a second term. However an election petition has been filed.",
-  image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5TSvhCp2qmH4KrdP3lPIDdxrbvVdlhFBbnaeuUBcqqyBnnoJvwQ",
+  image: "http://www.the-star.co.ke/sites/default/files/styles/new_full_content/public/articles/2017/02/09/1418453.jpg?itok=bmIcYFV3",
   author: "Osman Nderi",
   time: "2.50pm on August 9th"
-}
 }];
 
 export default Ember.Route.extend({
