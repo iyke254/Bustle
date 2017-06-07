0ed280101bc6236db33d3bd117d402b727426701
iyke <nderiony95@gmail.com>
2017-06-07 11:58:19 +0300

    included a news model

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,13 +1,24 @@
 import Ember from 'ember';
 
-var bustle = [{
-  headline: "How NASA lost the election",
+var news = [{
+  id: 1,
+  headline: "How NASA LOST the election",
   description: "After massive voter fraud the Jubilee coalition was elected back to power for a second term. However an election petition has been filed.",
   image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5TSvhCp2qmH4KrdP3lPIDdxrbvVdlhFBbnaeuUBcqqyBnnoJvwQ",
   author: "Najib Isaac",
   time: "11.50am on August 9th"
-  
-}]
+}, {
+  id: 2,
+  headline: "How Jubilee WON the election",
+  description: "After a lanslide victory Jubilee coalition was elected back to power for a second term. However an election petition has been filed.",
+  image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5TSvhCp2qmH4KrdP3lPIDdxrbvVdlhFBbnaeuUBcqqyBnnoJvwQ",
+  author: "Osman Nderi",
+  time: "2.50pm on August 9th"
+}
+}];
 
 export default Ember.Route.extend({
+  model(){
+    return news;
+  },
 });
