8e26de5e2e8f33bdb03123ddd9485962b0bef6d3
iyke <nderiony95@gmail.com>
2017-06-07 11:52:39 +0300

    added the data for the page

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
+var bustle = [{
+  headline: "How NASA lost the election",
+  description: "After massive voter fraud the Jubilee coalition was elected back to power for a second term. However an election petition has been filed.",
+  image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5TSvhCp2qmH4KrdP3lPIDdxrbvVdlhFBbnaeuUBcqqyBnnoJvwQ",
+  author: "Najib Isaac",
+  time: "11.50am on August 9th"
+  
+}]
+
 export default Ember.Route.extend({
 });
